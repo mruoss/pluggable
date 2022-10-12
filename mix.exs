dc96e55a6436af16b64eb2eb7f5495f443462109
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2022-10-12 10:39:56 +0200

    mix.exs

--- mix.exs
+++ mix.exs
@@ -1,18 +1,24 @@
 defmodule Pluggable.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/mruoss/pluggable"
+  @version "1.0.0"
+
   def project do
     [
       app: :pluggable,
-      version: "0.1.0",
+      description: "A Plug-like pipeline creator",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
+      package: package(),
       elixirc_paths: elixirc_paths(Mix.env()),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: cli_env(),
-      consolidate_protocols: Mix.env() != :test
+      consolidate_protocols: Mix.env() != :test,
+      dialyzer: dialyzer()
     ]
   end
 
@@ -43,7 +49,12 @@ defmodule Pluggable.MixProject do
     [
       # The main page in the docs
       main: "Pluggable.Token",
-      extras: ["README.md"]
+      source_ref: @version,
+      source_url: @source_url,
+      extras: [
+        "README.md",
+        "CHANGELOG.md"
+      ]
     ]
   end
 
@@ -57,6 +68,31 @@ defmodule Pluggable.MixProject do
       "coveralls.github": :test,
       "coveralls.xml": :test,
       "coveralls.json": :test
+    ]
+  end
+
+  defp package do
+    [
+      name: :pluggable,
+      maintainers: ["Michael Ruoss"],
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @source_url,
+        "Changelog" => "https://hexdocs.pm/pluggable/changelog.html"
+      },
+      extras: [
+        "README.md",
+        "CHANGELOG.md"
+      ],
+      files: ["lib", "mix.exs", "README.md", "LICENSE", "CHANGELOG.md", ".formatter.exs"]
+    ]
+  end
+
+  defp dialyzer do
+    [
+      ignore_warnings: ".dialyzer_ignore.exs",
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/pluggable.plt"}
     ]
   end
 end
